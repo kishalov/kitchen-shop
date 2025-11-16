a1eca293f25b12710b0400a3b745386eee9ea018
RedMatt <blyrror@gmail.com>
2025-11-16 18:34:22 +0400

    add OG, Favicon, snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,11 +25,11 @@ export const metadata: Metadata = {
 	icons: {
 		icon: [
 			{
-				url: "/favicon-light.svg",
+				url: "/favicon2.svg",
 				media: "(prefers-color-scheme: light)",
 			},
 			{
-				url: "/favicon-dark.svg",
+				url: "/favicon2.svg",
 				media: "(prefers-color-scheme: dark)",
 			},
 		],
